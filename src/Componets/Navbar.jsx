0999a6a4a80f498cbf72f6e06712d9100b843b6c
vigneshN6876 <nagarajvicky07@gmail.com>
2025-09-04 19:23:46 +0530

    first commit

---
+++ src/Componets/Navbar.jsx
@@ -0,0 +1,11 @@
+function Navbar() {
+    const course1 = "HTML"
+    return(
+        <nav>
+            <h2>Hello</h2>
+            <h1>{course1}</h1>
+            <button>login</button>
+        </nav>
+    );
+}
+export default Navbar
