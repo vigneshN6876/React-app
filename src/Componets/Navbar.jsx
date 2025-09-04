a5e0c7ee60f8e8ba5c0ba72428952f983bbe0ccd
vigneshN6876 <nagarajvicky07@gmail.com>
2025-09-04 19:32:18 +0530

    comit from lap

--- src/Componets/Navbar.jsx
+++ src/Componets/Navbar.jsx
@@ -3,6 +3,7 @@ function Navbar() {
     return(
         <nav>
             <h2>Hello</h2>
+            <h1>updated</h1>
             <h1>{course1}</h1>
             <button>login</button>
         </nav>
