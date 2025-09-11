032a6e681fd16227237da802a9e0a9586b49e6ee
vigneshN6876 <nagarajvicky07@gmail.com>
2025-09-11 07:21:15 +0530

    method as prop

--- src/Componets/CourseList.jsx
+++ src/Componets/CourseList.jsx
@@ -2,8 +2,9 @@ import html from '../assets/images.png'
 import css from '../assets/css.png'
 import js from '../assets/js.jpeg'
 import Courses from './Courses'
+import { useState } from 'react'
 function CourseList() {
-    const course = [
+    const [course , setCourse] = useState([
         {
             id : 1 , name : "HTML", price : 199, images : html ,rating : 5 
         },
@@ -16,10 +17,14 @@ function CourseList() {
         {
             id : 4 , name : "Node js", price :499 ,images : js, rating : 5
         }
-    ]
+    ])
     course.sort((a,b) => b.price - a.price)
-    const filterCourse = course.filter((c) => c.price <= 399)
-    const courseList = filterCourse.map((courseItem , index) => <Courses key = {index} name = {courseItem.name} price = {courseItem.price} images = {courseItem.images} rating = {courseItem.rating} />)
+    const handleDelete = (id) => {
+        const newCourse = course.filter((c) => c.id != id)
+        setCourse(newCourse)
+    }
+    // const filterCourse = course.filter((c) => c.price <= 399)
+    const courseList = course.map((courseItem ) => <Courses key = {courseItem.id} id = {courseItem.id} name = {courseItem.name} price = {courseItem.price} images = {courseItem.images} rating = {courseItem.rating} deletecourse = {handleDelete} />)
     return(
         <>
         {courseList}
--- src/Componets/Courses.jsx
+++ src/Componets/Courses.jsx
@@ -1,23 +1,35 @@
 import React from 'react'
 import html from '../assets/images.png'
 import PropTypes from 'prop-types'
+import { useState } from 'react'
 
 const Courses = ({
   images = html,
   name = "Default Course",
   price = 0,
   rating = 0,
-  
+  id,
+  deletecourse 
 }) => {
-    
-  
+    const [purchased , setPurchased] = useState(false)
+   
+    const BuyCourse = () => {
+        alert(`You have enrolled in ${name} course`)
+        console.log("You have enrolled in " + name + " course" + "Price is" + price);
+        setPurchased(true)
+
+    }
     return (
     <>
+      
        {name &&( <div className="card">
             <img src={images} alt="" />
             <h3>{name}</h3>
             <h3>${price}</h3>
-            <span>Rating {rating}</span>
+            <span>Rating {rating}</span> <br />
+            <button onClick={BuyCourse}>Enroll Now</button>
+            <p>{purchased ? "Already purchased" : "Enroll Now"}</p>
+            <button onClick={() => deletecourse(id)}>Delete</button>
         </div>)}
     </>
   )
